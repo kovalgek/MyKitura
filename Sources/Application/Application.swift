238d9c2dcf515cf3216d073c1595c62a55af3f01
Anton Kovalchuk <koval400k@gmail.com>
2019-12-26 18:01:25 +0100

    add routes

--- Sources/Application/Application.swift
+++ Sources/Application/Application.swift
@@ -1,8 +1,11 @@
 import Kitura
+import Dispatch
 
 public class App
 {
     let router = Router()
+    let workerQueue = DispatchQueue(label: "worker")
+
     
     public init() throws
     {
@@ -11,13 +14,19 @@ public class App
     
     func postInit() throws
     {
-        
+        initializeCodableRoutes(app: self)
     }
     
     public func run() throws
     {
         try postInit()
-        Kitura.addHTTPServer(onPort: 8000, with: router)
+        Kitura.addHTTPServer(onPort: 8080, with: router)
         Kitura.run()
+    }
+    
+    func execute(_ block: (() -> Void)) {
+        workerQueue.sync {
+            block()
+        }
     }
 }
